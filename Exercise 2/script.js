7cf0c954955cae6e09ab84cb20bb86b976a545af
Jake Mondragon <mondragonjake53@gmail.com>
2025-06-03 10:56:55 +0800

    Added functionality for buttons

---
+++ Exercise 2/script.js
@@ -0,0 +1,15 @@
+function about(){
+    alert("About Page should pop up")
+}
+
+function characters(){
+    alert("character page should pop up")
+}
+
+function seasons(){
+    alert("season page should pop up")
+}
+
+function watch(){
+    alert("watch page should pop up")
+}
